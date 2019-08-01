ad37c2762a37c8f3cca8b6d1d8e96e70ca5de2fd
Dominic <dominic.m.chu@gmail.com>
2019-08-01 07:40:07 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,5 +1,6 @@
 function callMe() {
   var lyric = "maybe";
+  lyric();
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
@@ -8,20 +9,17 @@ function callMe() {
 }
 
 function crazy() {
-  thisIsCrazy();
-
   var thisIsCrazy = function () {
     console.log("hey!!!")
   }
+  thisIsCrazy();
 }
 
 function sayMyName() {
   var name = "Cricky";
-
-  sayMy();
-
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
+  sayMy();
 }
